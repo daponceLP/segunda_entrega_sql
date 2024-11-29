9bd927b97090947aa4e9375ade9ccbe4ed1b230d
Dario Ponce <156376812+daponceLP@users.noreply.github.com>
2024-11-28 23:25:00 -0300

    Update 03_procedimientos.sql

--- objetos/03_procedimientos.sql
+++ objetos/03_procedimientos.sql
@@ -16,3 +16,28 @@ BEGIN
     SELECT LAST_INSERT_ID() AS new_proveedor_id;
 END //
 DELIMITER ;
+
+--Procedimiento para generar reporte de ventas en una fecha determinada
+
+USE supermercado;
+DROP STORED PROCEDURE IF EXISTS agregar_proveedor
+
+DELIMITER //
+CREATE PROCEDURE ventas_por_fecha(
+    IN p_fecha_inicio DATETIME,
+    IN p_fecha_fin DATETIME
+)
+BEGIN
+    SELECT 
+        v.fecha, 
+        a.nombre_producto, 
+        v.cantidad_sku, 
+        a.costo * v.cantidad_sku AS costo_total,
+        c.nombre_cliente,
+        c.tipo_facturacion
+    FROM ventas AS v
+    JOIN articulos AS a ON v.sku = a.sku
+    JOIN clientes AS c ON v.id_clientes = c.id_clientes
+    WHERE v.fecha BETWEEN p_fecha_inicio AND p_fecha_fin
+    ORDER BY v.fecha;
+END //
